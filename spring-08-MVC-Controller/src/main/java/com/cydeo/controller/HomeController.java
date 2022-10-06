b246f14ea5a94028fec0200faf9eb36b7a0beb4c
madiyard <icloudka@gmail.com>
2022-10-05 18:34:01 -0700

    2 st commit

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomePage(){
+        return "home.html";
+    }
+
+    @RequestMapping({"/example", "/register"})
+    public String getHomePage2(){
+        return "home.html";
+    }
+}
