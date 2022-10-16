a404cf614c8e398be4936a632061c263529c12a6
madiyard <icloudka@gmail.com>
2022-10-16 07:45:13 -0700

    3 st commit

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -12,6 +16,24 @@ public class StudentController {
 
         model.addAttribute("name", "Madiyar");
         model.addAttribute("course", "MVC");
+
+        String subject = "Spring boot";
+        model.addAttribute("subject", subject);
+
+        int id = new Random().nextInt();
+        model.addAttribute("id", id);
+
+        LocalDate dt = LocalDate.now();
+        model.addAttribute("dt", dt);
+
+        Student student = new Student(1, "Madiyar", "Damen");
+        model.addAttribute("student", student);
+
+
+
+
+
+
         return "student/welcome";
     }
 }
