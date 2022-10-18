fdeb2dcb104437017a5c0251beacee75740514f8
madiyard <icloudka@gmail.com>
2022-10-17 22:23:12 -0700

    6 st commit

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,22 +3,26 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register") // localhost:8080/student/register
+    //@RequestMapping(value = "/register", method = RequestMethod.GET) // localhost:8080/student/register
+    @GetMapping("/register")
     public String register( Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
 
-    @RequestMapping("/drop") // localhost:8080/student/drop
-    public String drop(){
 
-        return "student/register";
+    @RequestMapping("/welcome") // localhost:8080/student/welcome
+    @PostMapping("/welcome")
+    public String welcome(){
+
+
+        return "student/welcome";
     }
 }
